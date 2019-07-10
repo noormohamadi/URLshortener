0b108ee2b2b9a9c9ce581c0671a974ed6f1035a3
noormohamadi <noormohamadi@aut.ac.ir>
2019-07-10 11:21:42 +0430

    little problem with redirecting

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"../URLshortener/DB"
-	"../URLshortener/server"
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
@@ -10,9 +9,38 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 var db sql.DB
+
+func Redirect(w http.ResponseWriter, r *http.Request) {
+	//u := strings.Split(r.URL.String(), "/")
+	//fmt.Println(u[1])
+	params := mux.Vars(r)
+	shortLink := params["url"]
+	fmt.Println(shortLink)
+	var link string
+	err := db.QueryRow("SELECT url FROM urls WHERE shorten ='" + shortLink + "'").Scan(&link)
+
+	// Check for errors or invalid short urls
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatal(err)
+	} else if err != nil && err == sql.ErrNoRows {
+		log.Println(shortLink + " not found.")
+		w.Write([]byte("404 Not found."))
+	} else {
+		// Valid short url
+		log.Println("request: " + shortLink + " => " + link)
+		// do redirection
+		http.Redirect(w, r, link, http.StatusTemporaryRedirect)
+	}
+}
+
+func GetURL(w http.ResponseWriter, r *http.Request) {
+	u := strings.Split(r.URL.String(), "/")
+	fmt.Println(u[2])
+}
 
 func main() {
 	router := mux.NewRouter()
@@ -25,7 +53,7 @@ func main() {
 	}
 	router.SkipClean(true) // keep double slash for URLs like http://...
 	log.Println("Server starting...")
-	router.HandleFunc(`/get/{url:.*}`, server.GetURL).Methods("GET")
-	router.HandleFunc(`/re/{url:.*}`, server.Redirect).Methods("GET")
+	router.HandleFunc(`/get/{url:.*}`, GetURL).Methods("GET")
+	router.HandleFunc(`/{url:re.*}`, Redirect).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
