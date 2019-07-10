f73253db80b2225741145adbb8c2ee0d3051c1a2
noormohamadi <noormohamadi@aut.ac.ir>
2019-07-10 18:56:59 +0430

    get url and generate shorten url and add to db

--- main.go
+++ main.go
@@ -7,19 +7,23 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 var db sql.DB
+
+const expire = 20
 
 func Redirect(w http.ResponseWriter, r *http.Request) {
 	//u := strings.Split(r.URL.String(), "/")
 	//fmt.Println(u[1])
 	params := mux.Vars(r)
 	shortLink := params["url"]
-	fmt.Println(shortLink)
 	var link string
 	err := db.QueryRow("SELECT url FROM urls WHERE shorten ='" + shortLink + "'").Scan(&link)
 
@@ -32,6 +36,9 @@ func Redirect(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// Valid short url
 		log.Println("request: " + shortLink + " => " + link)
+		if link[0:1] != "//" && link[0:6] == "https:" && link[0:5] == "http:" {
+			link = "//" + link
+		}
 		// do redirection
 		http.Redirect(w, r, link, http.StatusTemporaryRedirect)
 	}
@@ -40,6 +47,26 @@ func Redirect(w http.ResponseWriter, r *http.Request) {
 func GetURL(w http.ResponseWriter, r *http.Request) {
 	u := strings.Split(r.URL.String(), "/")
 	fmt.Println(u[2])
+	sh := 0
+	for i := 0; i < len(u[2]); i++ {
+		sh += int(u[2][i])
+	}
+	fmt.Println(sh)
+	short := "re" + strconv.Itoa(sh) + strconv.Itoa(rand.Intn(100))
+	dat := time.Now().AddDate(0, 0, expire)
+	date := strings.Split(dat.String(), " ")[0]
+	log.Println(u[2] + " ==> " + short + " until : " + date)
+	add, err := db.Prepare("INSERT INTO urls(shorten, url, expire) VALUES(\"" + short + "\",\"" + u[2] + "\",'" + date + "')")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	defer add.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	add.Exec()
+
+	w.Write([]byte("yor shorten url : " + short))
 }
 
 func main() {
@@ -52,8 +79,18 @@ func main() {
 		os.Exit(-1)
 	}
 	router.SkipClean(true) // keep double slash for URLs like http://...
-	log.Println("Server starting...")
+	log.Println("Server available")
 	router.HandleFunc(`/get/{url:.*}`, GetURL).Methods("GET")
 	router.HandleFunc(`/{url:re.*}`, Redirect).Methods("GET")
+	router.HandleFunc(`/`, home)
 	log.Fatal(http.ListenAndServe(":5000", router))
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	//var index template.HTML
+	http.ServeFile(w, r, fmt.Sprintf("server/index.html"))
+	url := r.FormValue("uurl")
+	if url != "" {
+		fmt.Println(url)
+		http.Redirect(w, r, "get/"+url, http.StatusSeeOther)
+	}
 }
